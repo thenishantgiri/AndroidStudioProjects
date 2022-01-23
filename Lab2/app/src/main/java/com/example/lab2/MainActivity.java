8b88c577baa8e806f47200a8d0037282e3688026
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2022-01-23 18:56:17 +0530

    Lab 02

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    String tag = "Life cycle events!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.d(tag, "In the onStart() event");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        Log.d(tag, "In the onRestart() event");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(tag, "In the onResume() event");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(tag, "In the onPause() event");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(tag, "In the onStop() event");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
+    }
+}
